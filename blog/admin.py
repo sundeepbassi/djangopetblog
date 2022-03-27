65c7e4dc636f6c3c80403bb4e7b75f67cce2a363
sundeepbassi <sunnyeshe@yahoo.com>
2022-03-27 13:30:27 +0000

    Check code in admin file

--- blog/admin.py
+++ blog/admin.py
@@ -8,14 +8,13 @@ class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
