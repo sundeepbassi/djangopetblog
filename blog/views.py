7ff6cb86d40948fc7920d525af85ccaf4ff36617
sundeepbassi <sunnyeshe@yahoo.com>
2022-04-04 20:23:46 +0000

    Add update_comment file to templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 class PostList(generic.ListView):
@@ -76,3 +77,9 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+class UpdateCommentView(UpdateView):
+    model = Comment
+    template_name = 'update_comment.html'
+    fields = ['name', 'body']
