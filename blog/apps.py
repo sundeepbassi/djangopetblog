a34b9461e9ef7d1d5f01a1a63a73ca21ba307f1d
sundeepbassi <sunnyeshe@yahoo.com>
2022-04-06 13:23:19 +0000

    Removed warnings from apps file

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,8 @@
+"""This module is used for application configuration"""
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """This class is the configuration for the blog"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
