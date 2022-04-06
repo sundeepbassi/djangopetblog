efd47c8ed365b1253a179e557b4f7bda652c5448
sundeepbassi <sunnyeshe@yahoo.com>
2022-04-06 13:33:24 +0000

    Removed warnings from forms file

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,11 @@
-from .models import Comment
+"""This module specifies all of our forms code"""
 from django import forms
+from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    """This class specifies the comment form"""
     class Meta:
+        """This class specifies the comment body"""
         model = Comment
         fields = ('body',)
