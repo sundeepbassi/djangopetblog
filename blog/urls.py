2e77664deb40eb98e763d57fb47e6398b5df486d
sundeepbassi <sunnyeshe@yahoo.com>
2022-04-12 22:34:51 +0000

    Renamed functions About() and Gallery() to be lowercase, as per coding standards

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('about/', views.About, name='about'),
-    path('gallery/', views.Gallery, name='gallery'),
+    path('about/', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('comment/edit/<int:pk>',
